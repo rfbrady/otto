bc421bc6666227bb23d301bcd856eeabca02b586
rfbrady <rfbrady@email.wm.edu>
2018-07-30 13:29:49 -0400

    added logger

--- flaskapp2/app/__init__.py
+++ flaskapp2/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from config import Config
+import logging
 from flask_sqlalchemy import SQLAlchemy 
+from logging.handlers import RotatingFileHandler
+import os
 
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap 
@@ -17,3 +20,14 @@ migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 
 from app import routes, db, models
+
+
+if not app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/otto.log', maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s %(lineno)d ]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('otto startup')
